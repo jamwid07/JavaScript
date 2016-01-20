7967598e33765c93a064bc6f7947da3652deabc9
jamwid07 <jamwid07@mail.ru>
2016-01-20 23:17:41 +0300

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+// o'zgaruvchilar
+
+var name = "jamshid"; // satr tipli o'zgaruvchi
+var age = 22; // son tipli o'zgaruvchi
+var phone = 998939191613; // son tipli o'zgaruvchi
+var GENDER = "male"; // o'zgaruvchi katta xarflar bilan yozilsa konstanta deb qabul qiligan.
+
+alert (name);
+alert (age);
